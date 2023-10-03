1209a083aa240626330ee027b348b6cec497af40
Iulian <petruiulian01@gmail.com>
2023-10-04 02:24:05 +0300

    changed date format

--- frontend/src/components/AirQCard.jsx
+++ frontend/src/components/AirQCard.jsx
@@ -2,6 +2,7 @@
 function AirQCard({ station, onOpenId }) {
 
     const { strada, concentratie, updatedAt } = station;
+    const date = new Date(updatedAt).toLocaleString();
 
     return (
         <a href="#" className="block p-6 bg-white border border-gray-200 rounded-lg shadow-lg hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700 min-w-full min-h-full">
@@ -15,7 +16,7 @@ function AirQCard({ station, onOpenId }) {
                 {concentratie >= 300 ? <span className="px-2 py-1 font-bold text-maroon-800 bg-maroon-100 rounded-full">{concentratie} - Hazardous</span> : null}
                 {concentratie === '-' ? <span className="px-2 py-1 font-bold text-gray-800 bg-gray-100 rounded-full">No data</span> : null}
             </p>
-            {updatedAt ? <p className="text-sm font-normal text-gray-700 dark:text-gray-400">Last updated: {updatedAt}</p> : null}
+            {updatedAt ? <p className="text-sm font-normal text-gray-700 dark:text-gray-400">Last updated: {date}</p> : null}
         </a>
     )
 }
